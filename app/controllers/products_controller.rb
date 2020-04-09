ce1040e2648081913cf9f52851de941e07c26be9
oznekpark <kgog.33@gmail.com>
2020-04-09 10:45:04 +0900

    hamlの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,26 @@
 class ProductsController < ApplicationController
   
+  def home
+  end
+
   def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
