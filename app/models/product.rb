73c3824597def6252919f2181d9e1c5479c1fe82
oznekpark <kgog.33@gmail.com>
2020-04-13 16:20:18 +0900

    indexページのプルダウンメニューの準備

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,59 @@ class Product < ApplicationRecord
   belongs_to :smell_impression
   belongs_to :use_scene
   validates :name, :description, :image, presence: true
+
+  enum brand_id: {
+    "---": 0,
+    BURBERRY: 1,
+    DIOR: 2,
+    CHANEL: 3,
+    "JO MALONE": 4,
+  },  _prefix: true
+
+  enum sex_id: {
+    "---": 0,
+    "女性": 1,
+    "男性": 2,
+    "ユニセックス": 3,
+  },  _prefix: true
+
+  enum seoson_id: {
+    "---": 0,
+    "春": 1,
+    "夏": 2,
+    "秋": 3,
+    "冬": 4,
+  },  _prefix: true
+
+  enum smell_type_id: {
+    "---": 0,
+    "フルーティー": 1,
+    "フローラル": 2,
+    "ウッディ&オリエンタル": 3,
+    "シトラス": 4,
+    "シプレ&フゼア": 5,
+  },  _prefix: true
+
+  enum main_spice_id: {
+    "---": 0,
+  },  _prefix: true
+
+  enum smell_impression_id: {
+    "---": 0,
+    "エレガント": 1,
+    "キュート": 2,
+    "セクシー": 3,
+    "スマート": 4,
+    "ワイルド": 5,
+    "クール": 6,
+  },  _prefix: true
+
+  enum use_scene_id: {
+    "---": 0,
+    "オフィス": 1,
+    "デート": 2,
+    "デイリー": 3,
+    "パーティー": 4,
+    "リラックス": 5,
+  },  _prefix: true
 end
