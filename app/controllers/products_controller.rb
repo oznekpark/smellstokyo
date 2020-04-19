7acf3778b0bb9fbda0f5a9a43e7d1829e67b0fa0
oznekpark <kgog.33@gmail.com>
2020-04-19 19:26:09 +0900

    検索機能の実装
    1ページに二つのformがあるため、ルーティングにてcollectionを使用し、formそれぞれのルート、コントローラーのアクション、ビューページを作成した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,22 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.includes(:brand, :main_spice, :season, :sex, :smell_impression, :smell_type, :use_scene).order('created_at DESC')
-    
   end
 
   def show
     @product = Product.find(params[:id])
     @comments = @product.comments.includes(:user).order('created_at DESC')
     @comment = Comment.new
+  end
+
+  def searchFromHeadersForm
+    @products = Product.search(params[:keyword])
+  end
+
+  def searchFromPulldownsForm
+    @products = Product.get_sex_id(params[:sex_id])
+    # @products = Product.get_brand_id(params[:brand_id])
+    # @products = Product.get_brand_id(params[:brand_id]).get_sex_id(params[:sex_id]).get_smell_type_id(params[:smell_type_id]).get_main_spice_id(params[:main_spice_id]).get_smell_impression_id(params[:smell_impression_id]).get_use_scene_id(params[:use_scene_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root to: "products#home"
-  namespace :products do
-    resources :searches, only: :index
+  root to: 'products#home'
+  # namespace :products do
+  #   resources :searches, only: :index
+  # end
+  resources :products do
+    collection do
+      get 'searchFromHeadersForm'
+      get 'searchFromPulldownsForm'
+    end
   end
   resources :products do
     resources :comments, only: :create
