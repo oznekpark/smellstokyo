6a53accd41451073494fa3661f01125fba696984
oznekpark <kgog.33@gmail.com>
2020-04-20 08:35:43 +0900

    コントローラーのindexアクションにincludesメソッド記載

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,11 +8,11 @@ class ProductsController < ApplicationController
   end
 
   def indexOfWomenProducts
-    @products = Product.where(sex_id: 1).or(Product.where(sex_id: 3)).order('created_at DESC')
+    @products = Product.where(sex_id: 1).or(Product.where(sex_id: 3)).includes(:sex).order('created_at DESC')
   end
 
   def indexOfMenProducts
-    @products = Product.where(sex_id: 2).or(Product.where(sex_id: 3)).order('created_at DESC')
+    @products = Product.where(sex_id: 2).or(Product.where(sex_id: 3)).includes(:sex).order('created_at DESC')
   end
 
   def show
@@ -26,8 +26,7 @@ class ProductsController < ApplicationController
   end
 
   def searchFromPulldownsForm
-    @products = Product.get_sex_id(params[:sex_id]).order('created_at DESC')
-    # @products = Product.get_brand_id(params[:brand_id])
+    # @products = Product.get_sex_id(params[:sex_id]).order('created_at DESC')
     # @products = Product.get_brand_id(params[:brand_id]).get_sex_id(params[:sex_id]).get_smell_type_id(params[:smell_type_id]).get_main_spice_id(params[:main_spice_id]).get_smell_impression_id(params[:smell_impression_id]).get_use_scene_id(params[:use_scene_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'products#home'
-  # namespace :products do
-  #   resources :searches, only: :index
-  # end
   resources :products do
     collection do
       get 'searchFromHeadersForm'
