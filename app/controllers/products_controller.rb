df86c915440ce44ded71a9307e144acd0bea57d4
oznekpark <kgog.33@gmail.com>
2020-04-21 15:33:11 +0900

    product検索

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,7 @@ class ProductsController < ApplicationController
   end
 
   def searchFromPulldownsForm
+    @products = Product.get_brand_id(params[:brand_id]).get_sex_id(params[:sex_id]).get_smell_type_id(params[:smell_type_id]).get_main_spice_id(params[:main_spice_id]).get_smell_impression_id(params[:smell_impression_id]).get_use_scene_id(params[:use_scene_id])
     # @products = Product.get_sex_id(params[:sex_id]).order('created_at DESC')
     # @products = Product.get_brand_id(params[:brand_id]).get_sex_id(params[:sex_id]).get_smell_type_id(params[:smell_type_id]).get_main_spice_id(params[:main_spice_id]).get_smell_impression_id(params[:smell_impression_id]).get_use_scene_id(params[:use_scene_id])
   end
