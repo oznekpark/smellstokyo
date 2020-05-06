76d8695459fa7e4afdb8cd907001195b0e327c8f
oznekpark <kgog.33@gmail.com>
2020-05-07 02:47:21 +0900

    productコントローラーに商品一覧表示数を制限させる記述を追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index except: [:home, :index, :indexOfWomenProducts, :indexOfMenProducts, :show, :searchFromHeadersForm, :searchFromPulldownsForm]
   
   def home
+    @products = Product.last(6)
   end
 
   def index
@@ -29,5 +31,10 @@ class ProductsController < ApplicationController
     @products = Product.get_brand_id(params[:brand_id]).get_sex_id(params[:sex_id]).get_smell_type_id(params[:smell_type_id]).get_main_spice_id(params[:main_spice_id]).get_smell_impression_id(params[:smell_impression_id]).get_use_scene_id(params[:use_scene_id])
     # @products = Product.get_sex_id(params[:sex_id]).order('created_at DESC')
     # @products = Product.get_brand_id(params[:brand_id]).get_sex_id(params[:sex_id]).get_smell_type_id(params[:smell_type_id]).get_main_spice_id(params[:main_spice_id]).get_smell_impression_id(params[:smell_impression_id]).get_use_scene_id(params[:use_scene_id])
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :home unless user_signed_in?
   end
 end
