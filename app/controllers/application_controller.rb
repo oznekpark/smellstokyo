7014cac555939d6dae3ea7a046a3bc6874a2bbec
oznekpark <kgog.33@gmail.com>
2020-05-12 18:10:18 +0900

    未ログイン時でもカート機能が保持できるようdeviseコントローラーを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,27 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+    render file: "#{Rails.root}/public/403.html", status: 403, layout: false
     ## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
     ## this render call should be:
     # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
   private
   #セッションの作成
   def current_cart
     # セッションから取得したcart_idを元にCartテーブルからCart情報を取得
-    @current_cart = Cart.find_by(id: session[:cart_id])
+    current_cart = Cart.find_by(id: session[:cart_id])
     # Cart情報が存在しない場合、@current_cartを作成
-    @current_cart = Cart.create unless @current_cart
+    current_cart = Cart.create unless current_cart
     # 取得したCart情報よりIDを取得し、セッションに設定
-    session[:cart_id] = @current_cart.id
+    session[:cart_id] = current_cart.id
     # Cart情報を返却
-    @current_cart
-  end
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    current_cart
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   
   root 'products#home'
   
