fd320f7792116f2cbbf13d1fbb537be30c015fdc
oznekpark <kgog.33@gmail.com>
2020-05-18 02:33:24 +0900

    エラーメッセージの日本語化

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,7 +8,7 @@ class Address < ApplicationRecord
   validates :prefecture_code, :address_city, :address_street, presence: true
   validates :destination_first_name, :destination_family_name, format: { with: VALID_NAME_REGEX }, presence: true
   validates :destination_first_name_kana, :destination_family_name_kana, format: { with: VALID_KANA_REGEX }, presence: true
-  validates :postcode, presence: true, numericality: { only_integer: true }
+  validates :postcode, presence: true, numericality: { only_integer: true },  format: {with: /\A[0-9]{3}[0-9]{4}\z/}
 
   def postcode=(value)
     value.tr!('０-９', '0-9') if value.is_a?(String)
