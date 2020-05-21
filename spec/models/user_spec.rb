c11f12407c1773a8245e0f1cebfe4e9f0da3fe48
oznekpark <kgog.33@gmail.com>
2020-05-21 23:20:12 +0900

    factory.botを導入して、テストコードの書き換えを実施

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,31 +3,30 @@ describe User do
   describe '#create' do
 
     it "全てが存在すれば登録できること" do
-      user = User.new(nickname: "tester", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
+      user = build(:user)
       expect(user).to be_valid
     end
 
-    it "nicknameがない場合は登録できないこと" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+    it "nikcnameがない場合は登録できないこと" do
+      user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("を入力してください")
     end
 
     it "emailがない場合は登録できないこと" do
-      user = User.new(nickname: "tester", email: "", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
 
     it "passwordがない場合は登録できないこと" do
-      user = User.new(nickname: "tester", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
+      user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
 
     it "確認用passwordがない場合は登録できないこと" do
-      user = User.new(nickname: "tester", email: "kkk@gmail.com", password: "00000000", password_confirmation: "")
+      user = build(:user, password_confirmation: nil)
       user.valid?
       expect(user.errors[:password_confirmation]).to include("を入力してください")
     end
@@ -39,13 +38,7 @@ describe User do
     end
 
     it "nicknameが20文字以上は登録できないこと" do
-      user = User.new(nickname: "tester00112233445566778899", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors[:nickname]).to include("は20文字以内で入力してください")
-    end
-
-    it "nicknameが20文字以上は登録できないこと" do
-      user = User.new(nickname: "tester00112233445566778899", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "tester00112233445566778899")
       user.valid?
       expect(user.errors[:nickname]).to include("は20文字以内で入力してください")
     end
