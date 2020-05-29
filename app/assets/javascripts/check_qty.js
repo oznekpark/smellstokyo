8b1914db343f7b93f32b28689eed2866e68633cc
oznekpark <kgog.33@gmail.com>
2020-05-29 23:05:33 +0900

    商品をカート投入時に数量選択がない場合に入力指示表示させるJSを作成

---
+++ app/assets/javascripts/check_qty.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("#addToCart").on('click', function(e){
+    if(!(document.getElementById('quantity').value)){
+      alert("数量を選択してください。");
+      return false;
+    }else{
+      return true;
+    }
+  })
+});
