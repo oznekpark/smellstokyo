b1b946e4abadc47e668c3b6ee38bfe664b51c9b2
oznekpark <kgog.33@gmail.com>
2020-06-01 21:09:12 +0900

    select画面でブランドなど選択されていない際の処理追加
    具体的には、JSファイルを作成し、プルダウンメニューよりカテゴリー選択がない場合に項目選択をするように促すポッポアップを表示させる。

---
+++ app/assets/javascripts/selectProducts.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    $("#selectBtn").on("click", function(e){
+      if(document.getElementById('brand_id').value === "" && document.getElementById('sex_id').value === "" && document.getElementById('smell_type_id').value === "" && document.getElementById('main_spice_id').value === "" && document.getElementById('smell_impression_id').value === "" && document.getElementById('use_scene_id').value === ""){
+        alert("項目を選択してください。");
+        return false;
+      }else{
+        return true;
+      }
+    })
+  });
+});
