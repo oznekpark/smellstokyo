a85c543b06a3563597e5828d18846ea5a6dd5712
oznekpark <kgog.33@gmail.com>
2020-06-06 20:04:18 +0900

    商品一覧、商品詳細ページにおけるパンくず機能の実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,23 @@
 crumb :root do
-  link "Home", root_path
+  link "HOME", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :products do
+  link "SELECT", products_path
+  parent :root
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :womenproducts do
+  link "WOMEN", indexOfWomenProducts_products_path
+  parent :root
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :menproducts do
+  link "MEN", indexOfMenProducts_products_path
+  parent :root
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :product do |product|
+  link product.namejap, product_path(product)
+  parent :products, :womenproducts, :menproducts
+end
