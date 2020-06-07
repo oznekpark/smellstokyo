493fb9e7813a250044497712ed86ac746a76d6c2
oznekpark <kgog.33@gmail.com>
2020-06-07 23:04:33 +0900

    ユーザーマイページにパンくず機能実装準備

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -21,3 +21,37 @@ crumb :product do |product|
   link product.namejap, product_path(product)
   parent :products, :womenproducts, :menproducts
 end
+
+crumb :mypage do
+  link "MY ACCOUNT", user_path(current_user)
+end
+
+crumb :favorite do
+  link "お気に入り", bookmarks_path
+  parent :mypage
+end
+
+crumb :order do
+  link "注文履歴", orders_path
+  parent :mypage
+end
+
+crumb :orderdetail do |order|
+  link "注文履歴詳細", order_details_path(order)
+  parent :order
+end
+
+crumb :address do
+  link "送付先住所", addresses_path
+  parent :mypage
+end
+
+crumb :card do
+  link "支払い方法", new_card_path
+  parent :mypage
+end
+
+crumb :accountmodify do
+  link "アカウント修正", edit_user_registration_path
+  parent :mypage
+end
